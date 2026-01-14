ed01ebab3b79934cbb724970a825d5e149345896
khalid ghiboub <khalid.ghiboub@gmail.com>
2026-01-14 13:45:40 +0400

    Release v0.1.3

--- livekit/plugins/faseeh/version.py
+++ livekit/plugins/faseeh/version.py
@@ -1,4 +1,4 @@
-# Copyright 2023 LiveKit, Inc.
+# Copyright 2026 Actualize, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
